8de418a3c31e172ec972d46118f09142a7acb991
Janek Lasocki-Biczysko <j.lasocki-biczysko@intrallect.com>
2012-11-09 11:35:01 +0000

    adding filter

---
+++ README.md
@@ -0,0 +1,14 @@
+# JSKit
+
+Load common JavaScript libraries into Chrome tabs/windows
+
+## Changes
+
+0.2.3
+
+- added proper version numbers for Underscore.js and Backbone.js
+- added filter
+
+0.2.2
+
+- properly capturing / restoring "loaded" state of libraries per tab
--- popup.js
+++ popup.js
@@ -1,5 +1,18 @@
 $(function() {
 
+    var Filter = Backbone.View.extend({
+        events: {
+            "keyup": function() {
+                var val = this.$el.val().toLowerCase();
+                console.log('keyup triggered', val == '');
+                this.options.collection.each(function(model) {
+                    var show = val == '' || model.get('label').toLowerCase().indexOf(val) != -1;
+                    model.trigger('show', show);
+                });
+            }
+        }
+    });
+
 	var ScriptView = Backbone.View.extend({
 		tagName: 'li',
 		className: 'script',
@@ -11,11 +24,12 @@ $(function() {
 		initialize: function() {
 			// refresh on loaded events
 			this.model.on('change:isLoaded', this.render, this);
+			this.model.on('show', function(show) {  this.$el.toggle(show); }, this);
 		},
 		render: function() {
 			this.$el.empty();
 
-			var label = this.make('span', {title: 'version:' + this.model.get('version')}, this.model.get('label'));
+			var label = this.make('span', {}, this.model.get('label') + ' <em>' + this.model.get('version') + '</em>');
 			this.$el.append(label);
 
 			if (this.model.get('isLoaded')) {
@@ -34,27 +48,36 @@ $(function() {
 
 	var ScriptsView = Backbone.View.extend({
 		initialize: function() {
-
-			var that = this;
-
-			chrome.tabs.getSelected(null, function(tab) {
-				chrome.extension.sendRequest({
-					action: "getScripts",
-					tab: tab
-				},
-				function(scripts) {
-					that.scripts = new Scripts(scripts, {tabId: tab.id});
-					that.scripts.on("change:isLoaded", function() {
-						chrome.extension.sendRequest({
-							action: "setScripts",
-							tab: tab,
-							scripts: this.toJSON()
-						});
-					});
-					that.render();
-				});
-			});
+		    _.bindAll(this, 'setScripts', 'loadTab');
+			chrome.tabs.getSelected(null, this.loadTab);
 		},
+
+		loadTab: function(tab) {
+
+		    this.scripts = new Scripts([], {tabId: tab.id});
+		    this.scripts.on("change:isLoaded", function() {
+                chrome.extension.sendRequest({
+                    action: "setScripts",
+                    tab: tab,
+                    scripts: this.toJSON()
+                });
+            });
+            chrome.extension.sendRequest({
+                action: "getScripts",
+                tab: tab
+            }, this.setScripts);
+
+            // hook up the filter
+            this.filter = new Filter({
+                el: $('#filter'),
+                collection: this.scripts
+            });
+		},
+
+		setScripts: function(scripts) {
+		    this.scripts.reset(scripts);
+            this.render();
+        },
 
 		render: function() {
 			var el = this.el;
@@ -67,4 +90,5 @@ $(function() {
 	});
 
 	new ScriptsView({el: $('#scripts_list')});
+	console.log('loaded!');
 });
